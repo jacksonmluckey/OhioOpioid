1aa6fc356e83982f9fff488337e4b9dc4b068c18
Jackson Luckey <jacksonmluckey@gmail.com>
2019-05-22 13:42:59 -0400

    successfully refactored into function in 1 try (nice)

--- PDFtables.R
+++ PDFtables.R
@@ -11,24 +11,28 @@ library('tabulizer')
 # Load all tables from PDF
 raw_tables <- extract_tables("2017_OhioDrugOverdoseReport.pdf")
 
-# Extracting "Table 3. Number of Unintentional Drug Overdose Deaths and Average Crude and Age-Adjusted Annual Death Rates Per 100,000 Population, by County, 2005-2017"
-table7 <- raw_tables[7] %>%
-  map(as_data_frame) %>%
-  bind_rows() %>%
-  mutate(V18 = NULL)
+unintentional_overdose_county_year <- function(table) {
+  # Extracting "Table 3. Number of Unintentional Drug Overdose Deaths and Average Crude and Age-Adjusted Annual Death Rates Per 100,000 Population, by County, 2005-2017"
+  table <- table %>%
+    map(as_data_frame) %>%
+    bind_rows() %>%
+    mutate(V18 = NULL)
+  
+  # Hacky solution to the uncallable blank column names problem
+  table[1,16] <- "TotalToMerge"
+  table[1,19] <- "AARtoMerge"
+  
+  # Change first row into column names
+  colnames(table) <- as.character(table[1,])
+  table <- table[-1,]
+  
+  # Drop unnecessary columns, combine the split ones, and rename year columns
+  table <- table %>%
+    mutate("Age Adjusted Rate" = NULL) %>%
+    rename("AgeAdjustedRate" = "AARtoMerge") %>%
+    rename("CrudeRate" = "Crude Rate") %>%
+    unite("Total2012to2017", "2012-2017 Total", "TotalToMerge", sep = "") %>%
+    rename_at(vars(starts_with("2")), list( ~str_replace(., "2", "Year2")))
+}
 
-# Hacky solution to the uncallable blank column names problem
-table7[1,16] <- "TotalToMerge"
-table7[1,19] <- "AARtoMerge"
-
-# Change first row into column names
-colnames(table7) <- as.character(table7[1,])
-table7 <- table7[-1,]
-
-# Drop unnecessary columns, combine the split ones, and rename year columns
-table8 <- table7 %>%
-  mutate("Age Adjusted Rate" = NULL) %>%
-  rename("AgeAdjustedRate" = "AARtoMerge") %>%
-  rename("CrudeRate" = "Crude Rate") %>%
-  unite("Total2012to2017", "2012-2017 Total", "TotalToMerge", sep = "") %>%
-  rename_at(vars(starts_with("2")), list( ~str_replace(., "2", "Year2")))
+table1 <- unintentional_overdose_county_year(raw_tables[7])
