89c90006efb34b24598e24f34e0af039521b6df8
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-09-19 18:06:48 -0700

    uploading past changes to TransferReceipts.R

--- R/PDF-Extractors/TransferReceipts.R
+++ R/PDF-Extractors/TransferReceipts.R
@@ -10,6 +10,10 @@ raw_pdf_extraction <- extract_tables(here("Data", "PDFs", "OhioCountyIndicators.
 TransferReceipts <- bind_rows(raw_pdf_extraction[1], raw_pdf_extraction[2])
 
 # Split the accidentally combined 2014 2015 column
+TransferReceipts <- TransferReceipts %>%
+  separate(X.3, c("TotalTransferReceipts2014", "TotalTransferReceipts2015"), sep = " ")
 
 # Rename the columns
-names(TransferReceipts) <- c("Area", "TotalTransferReceipts2012")
+names(TransferReceipts) <- c("Area", "TotalTransferReceipts2012", "TotalTransferReceipts2013", "TotalTransferReceipts2014",
+                             "TotalTransferReceipts2015", "TotalTransferReceipts2016", "TotalTransferReceipts2017",
+                             "TotalIncome", "PercentTotalIncome", "DependencyRank")
