fa93992a4b674592260c3ca4746384234df65d70
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-09-21 10:34:44 -0700

    working on extracting table A1 from the detailed tables section

--- R/PDF-Extractors/OhioPoverty.R
+++ R/PDF-Extractors/OhioPoverty.R
@@ -4,6 +4,9 @@ library(tabulizer)
 
 # This script will pull all relevant tables out of "OhioCountyPoverty.pdf"
 
-# Load the PDF file and extract every table from it
+# Load the PDF file and extract every table from the detailed tables section
 pdf_path <- here("Data", "PDFs", "OhioCountyPoverty.pdf")
-raw_tables <- extract_tables(pdf_path)
+raw_tables <- extract_tables(pdf_path, pages = 58:95)
+
+# Table A1: Number and Percent of Poor Persons in Ohio and the U.S., 1959, 1969-2017 (in Thousands, Except for Percentages)
+PoorPersons <- as.tibble(raw_tables[[1]])
