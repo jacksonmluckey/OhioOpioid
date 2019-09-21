7c79132f7bc6ee6d8da107b362edba8cd82e3d17
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-09-21 10:24:37 -0700

    extracting all tables from OhioCountyPoverty.pdf

--- R/PDF-Extractors/OhioPoverty.R
+++ R/PDF-Extractors/OhioPoverty.R
@@ -3,3 +3,7 @@ library(here)
 library(tabulizer)
 
 # This script will pull all relevant tables out of "OhioCountyPoverty.pdf"
+
+# Load the PDF file and extract every table from it
+pdf_path <- here("Data", "PDFs", "OhioCountyPoverty.pdf")
+raw_tables <- extract_tables(pdf_path)
