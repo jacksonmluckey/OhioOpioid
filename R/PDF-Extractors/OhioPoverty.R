971986a0b4d530f9c9fc31206ef14b29c5cd257f
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-09-21 11:24:56 -0700

    changed PoorPersons to PoorUnits

--- R/PDF-Extractors/OhioPoverty.R
+++ R/PDF-Extractors/OhioPoverty.R
@@ -12,54 +12,54 @@ raw_tables <- extract_tables(pdf_path, pages = 58:95)
 #############################################################################################################################
 # Table A1: Number and Percent of Poor Persons in Ohio and the U.S., 1959, 1969-2017 (in Thousands, Except for Percentages) #
 #############################################################################################################################
-PoorPersons <- as.tibble(raw_tables[[1]])
+PoorUnits <- as.tibble(raw_tables[[1]])
 # Remove bad formatting rows
-PoorPersons <- PoorPersons[4:nrow(PoorPersons),]
-# Split PoorPersons in half (because it has the rows duplicated to fit all years on one page)
-PoorPersons1 <- PoorPersons[,1:7]
-PoorPersons2 <- PoorPersons[,8:14]
+PoorUnits <- PoorUnits[4:nrow(PoorUnits),]
+# Split PoorUnits in half (because it has the rows duplicated to fit all years on one page)
+PoorUnits1 <- PoorUnits[,1:7]
+PoorUnits2 <- PoorUnits[,8:14]
 # Rename the columns
-PoorPersonsColNames <- c("Year", "OhioTotal", "OhioPoorTotal", "OhioPercentPoor", "USTotal", "USPoorTotal", "USPercentPoor")
-names(PoorPersons1) <- PoorPersonsColNames
-names(PoorPersons2) <- PoorPersonsColNames
+PoorUnitsColNames <- c("Year", "OhioTotal", "OhioPoorTotal", "OhioPercentPoor", "USTotal", "USPoorTotal", "USPercentPoor")
+names(PoorUnits1) <- PoorUnitsColNames
+names(PoorUnits2) <- PoorUnitsColNames
 # Recombine the tibbles
-PoorPersons <- bind_rows(PoorPersons1, PoorPersons2)
+PoorUnits <- bind_rows(PoorUnits1, PoorUnits2)
 # Remove the leftovers
-rm(PoorPersons1)
-rm(PoorPersons2)
+rm(PoorUnits1)
+rm(PoorUnits2)
 # Remove extra characters (eg #^$, ) from the numerical columns
-PoorPersons <- PoorPersons %>%
+PoorUnits <- PoorUnits %>%
   mutate_all(parse_number)
 # Remove pointless rows (those without a year)
-PoorPersons <- PoorPersons %>%
+PoorUnits <- PoorUnits %>%
   filter(! is.na(Year))
 ######################################################
 # Reshape that tibble so that US/Ohio becomes a column
-# DO this by splitting PoorPersons into 2 tibbles
+# Do this by splitting PoorUnits into 2 tibbles
 # Renaming their columns and reordering them
 # And then recombining the tibbles
 #####################################################
-PoorPersonsUS <- PoorPersons[,5:7]
-PoorPersonsUS$Year <- c(1959, 1969:2016)
-PoorPersonsUS$Area <- "US"
-PoorPersonsOhio <- PoorPersons[,1:4]
-PoorPersonsOhio$Area <- "Ohio"
-# Reorder the PoorPersonsOhio columns so that the names and values will line up after applying the tidy names
-PoorPersonsOhio <- PoorPersonsOhio %>%
+PoorUnitsUS <- PoorUnits[,5:7]
+PoorUnitsUS$Year <- c(1959, 1969:2016)
+PoorUnitsUS$Area <- "US"
+PoorUnitsOhio <- PoorUnits[,1:4]
+PoorUnitsOhio$Area <- "Ohio"
+# Reorder the PoorUnitsOhio columns so that the names and values will line up after applying the tidy names
+PoorUnitsOhio <- PoorUnitsOhio %>%
   select(OhioTotal, OhioPoorTotal, OhioPercentPoor, Year, Area)
-TidyPoorPersonsColNames <- c("Total", "Poor", "PercentPoor", "Year", "Area")
-names(PoorPersonsOhio) <- TidyPoorPersonsColNames
-names(PoorPersonsUS) <- TidyPoorPersonsColNames
-PoorPersons <- bind_rows(PoorPersonsOhio, PoorPersonsUS)
+TidyPoorUnitsColNames <- c("Total", "Poor", "PercentPoor", "Year", "Area")
+names(PoorUnitsOhio) <- TidyPoorUnitsColNames
+names(PoorUnitsUS) <- TidyPoorUnitsColNames
+PoorUnits <- bind_rows(PoorUnitsOhio, PoorUnitsUS)
 # Remove the leftovers
-rm(PoorPersonsOhio)
-rm(PoorPersonsUS)
+rm(PoorUnitsOhio)
+rm(PoorUnitsUS)
 # Write to CSV
-write_csv(PoorPersons, path = here("Data", "CSV", "PoorPersons.csv"))
+write_csv(PoorUnits, path = here("Data", "CSV", "PoorPersons.csv"))
 # Clean up my global environment
-rm(PoorPersons)
-rm(PoorPersonsColNames)
-rm (TidyPoorPersonsColNames)
+rm(PoorUnits)
+rm(PoorUnitsColNames)
+rm (TidyPoorUnitsColNames)
 
 
 ##############################################################################################################################
